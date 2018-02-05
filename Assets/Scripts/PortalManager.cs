627b325e389a10abdfb431d92a2d35f89df64b90
apelofff <joergenstolt@gmail.com>
2018-02-05 13:54:56 +0100

    Testing med portals.

---
+++ Assets/Scripts/PortalManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalManager : MonoBehaviour
+{
+
+    public List <GameObject> nextPortal = new List<GameObject>();
+    public Vector3 pos;
+    public int portalCounter;
+    public bool isInTrigger;
+
+    private void Start()
+    {
+        portalCounter = 0;
+        pos = nextPortal[portalCounter].transform.position;
+    }
+
+    private void Update()
+    {
+        Debug.Log(transform.position);
+    }
+
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "portal")
+        {
+            transform.position = pos;
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+    }
+
+
+}
