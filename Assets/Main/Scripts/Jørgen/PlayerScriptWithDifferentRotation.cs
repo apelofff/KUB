957d400a20651f828d6ad8b123ed8dea6e92aa27
apelofff <joergenstolt@gmail.com>
2018-02-13 15:56:30 +0100

    + null gravity & bytte av rotasjon.

--- Assets/Main/Scripts/Jørgen/PlayerScriptWithDifferentRotation.cs
+++ Assets/Main/Scripts/Jørgen/PlayerScriptWithDifferentRotation.cs
@@ -11,22 +11,24 @@ using UnityEngine.SceneManagement;
 
 
 
-public class PlayerControllers : MonoBehaviour {
+public class PlayerScriptWithDifferentRotation : MonoBehaviour
+{
 
     [Header("For shooting")]
     public float shootingSpeed;
     private int ID;
 
-    [SerializeField]private Transform ThisTransform;
-    [SerializeField]private Rigidbody ThisRB = null;
-    [SerializeField] private Quaternion newRot =  Quaternion.Euler(10f, 0f, 0f);
+    [SerializeField] private Transform ThisTransform;
+    [SerializeField] private Rigidbody ThisRB = null;
+    [SerializeField] private Quaternion newRot = Quaternion.Euler(10f, 0f, 0f);
 
 
-    [SerializeField]private bool switching = false;
+    [SerializeField] private bool switching = false;
 
     [Header("Other cube properties")]
-    [SerializeField]private Vector3 scaleSize ;
-    [SerializeField]public Vector3 decreasingValue;
+    [SerializeField]
+    private Vector3 scaleSize;
+    [SerializeField] public Vector3 decreasingValue;
     public float rotationSpeedZ = 1f;
     public bool rotatateDirection;
 
@@ -36,12 +38,12 @@ public class PlayerControllers : MonoBehaviour {
 
     [Header("Objects outside the game")]
     public GameObject targetArrow;
-    public Transform otherPlayer; 
+    public Transform otherPlayer;
 
     [Header("Juicy")]
     public SlowMotion slowMotion;
     public CameraShake cameraShake;
-  
+
 
     [Header("Players")]
     public GameObject[] gameobjects;
@@ -49,14 +51,16 @@ public class PlayerControllers : MonoBehaviour {
 
     [SerializeField] bool StopMotion = false;
 
-    
+
     // proppeling the player foward using rb or the transform
     // the player van press space again to proppel the body forward, then body is kinematic and enable the animation Arrow.
     // the arrow needs to be the foward vector or rigth vector. only needing to rotate the cube.
-    
 
-	// Use this for initialization
-	void Start () {
+
+    // Use this for initialization
+    void Start()
+    {
+        rotatateDirection = true;
         ThisTransform = GetComponent<Transform>();
         ThisRB = GetComponent<Rigidbody>();
         ThisRB.isKinematic = true;
@@ -70,25 +74,34 @@ public class PlayerControllers : MonoBehaviour {
 
 
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
 
         // This is for the scaling down over time
 
         timeFloat -= Time.deltaTime;
-        if(timeFloat>0)
-            scaleSize  = new Vector3(timeFloat / scaleDecrease, timeFloat / scaleDecrease, timeFloat / scaleDecrease);
-        ThisTransform.localScale = scaleSize ;
+        if (timeFloat > 0)
+            scaleSize = new Vector3(timeFloat / scaleDecrease, timeFloat / scaleDecrease, timeFloat / scaleDecrease);
+        ThisTransform.localScale = scaleSize;
         if (Input.GetButton("Jump") && ID == 1)
         {
             ThisRB.isKinematic = true;
 
         }
-        else if (Input.GetButtonUp("Jump") && StopMotion == true && ID == 1) {
-            ThisRB.isKinematic = false; 
+        else if (Input.GetButtonUp("Jump") && StopMotion == true && ID == 1 && rotatateDirection == true)
+        {
+            ThisRB.isKinematic = false;
             ThisRB.AddForce(transform.right * shootingSpeed);
             rotatateDirection = false;
+        }
+
+        else if (Input.GetButtonUp("Jump") && StopMotion == true && ID == 1 && rotatateDirection == false)
+        {
+            ThisRB.isKinematic = false;
+            ThisRB.AddForce(transform.right * shootingSpeed);
+            rotatateDirection = true;
         }
 
         if (Input.GetButtonDown("Jump") && ID == 0)
@@ -99,10 +112,16 @@ public class PlayerControllers : MonoBehaviour {
 
 
         // SlowMotion state, and aiming state
-        if (ThisRB.isKinematic == true && StopMotion == true && ID == 1)
+        if (ThisRB.isKinematic == true && StopMotion == true && ID == 1 && rotatateDirection == true)
         {
             targetArrow.SetActive(true);
             transform.Rotate(0, 0, Time.deltaTime * rotationSpeedZ);
+        }
+
+        else if (ThisRB.isKinematic == true && StopMotion == true && ID == 1 && rotatateDirection == false)
+        {
+            targetArrow.SetActive(true);
+            transform.Rotate(0, 0, Time.deltaTime * -rotationSpeedZ);
         }
 
         else
@@ -111,11 +130,11 @@ public class PlayerControllers : MonoBehaviour {
         if (Input.GetButtonDown("Jump") && StopMotion == false && ID == 1)
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
-   
+
     public void OnTriggerEnter(Collider other)
     {
-        
-        if(other.tag == "Player")
+
+        if (other.tag == "Player")
         {
 
             float add = other.transform.localScale.x * scaleDecrease;
@@ -127,10 +146,10 @@ public class PlayerControllers : MonoBehaviour {
 
         //________________________________________________________________________________________________________________________
 
-        if(other.tag == "Dead")
+        if (other.tag == "Dead")
         {
             cameraShake.shouldShake = true;
-            StopMotion = false; 
+            StopMotion = false;
             ThisRB.isKinematic = true;
 
         }
@@ -138,7 +157,7 @@ public class PlayerControllers : MonoBehaviour {
 
     void Littlejump()
     {
-     
+
         ThisRB.AddForce(transform.up * shootingSpeed);
         StopMotion = true;
         // slowMotion.slowDownActive = true; 
