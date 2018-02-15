f8fe1641c4c3539364610c8e66b6158fb365e8c6
anders findal <anders.fj@gmail.com>
2018-02-16 00:03:30 +0100

    Tutorial lvl 1 og 2 er ferdig, men jeg har noe problemer med animasjonene i lvl3. Mer presist så er det programmering av animasjonene i lvl3 jeg har problemer med.

---
+++ Assets/Main/Anim/TutorialAnimations 1/AnimationScript/ObstacleMover.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleMover : MonoBehaviour
+{
+    bool animBool;
+    Animator animator;
+    float flipCounter;
+    public float lengthOfAnimation = 3;
+
+	void Start ()
+    {
+        animator = this.GetComponent<Animator>();
+        animBool = animator.GetBool("isUp");
+        InvokeRepeating("Flipper",lengthOfAnimation,lengthOfAnimation);
+    }
+	
+    void Flipper()
+    {
+        if (animBool == false)
+        {
+            animBool = true;
+            animator.SetBool("isUp", true);
+        }
+        else if (animBool == true)
+        {
+            animBool = false;
+            animator.SetBool("isUp", false);
+        }
+    }
+}
