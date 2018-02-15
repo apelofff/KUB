f2b014cb0bbfa8443713fd57a6b7b1800fe15857
MSI\Tommas <havremann@gmail.no>
2018-02-15 14:17:44 +0000

    Semi-Working highscore in scene

--- Assets/Main/Scripts/Tommas/T_HighScoreSystem.cs
+++ Assets/Main/Scripts/Tommas/T_HighScoreSystem.cs
@@ -5,26 +5,38 @@ using UnityEngine.UI;
 
 public class T_HighScoreSystem : MonoBehaviour {
 
-    public Text HighScore;
-    public float LocalTime;
+    [SerializeField] private float currentTimeOnThisLevel = 1 ;
+ 
+
+    public bool IsTimerOn; 
+
+    public Text text;
+    public Text Highscore;
+
 
     private void Start()
     {
-
+        Highscore.text = PlayerPrefs.GetFloat("Highscore", 1).ToString();   
     }
-
-    private void Update()
+    
+    void Update()
     {
-        LocalTime =+ Time.deltaTime;
-        HighScore.text = PlayerPrefs.GetInt("HighScore", 0).ToString();
+        
+        if (IsTimerOn == true)
+        {
+            currentTimeOnThisLevel = Time.time;
+            currentTimeOnThisLevel = Mathf.Round(currentTimeOnThisLevel);
+            text.text = currentTimeOnThisLevel.ToString();
+            if (currentTimeOnThisLevel > PlayerPrefs.GetFloat("Highscore", 1))
+            {
+                PlayerPrefs.SetFloat("Highscore", currentTimeOnThisLevel);
+                Highscore.text = currentTimeOnThisLevel.ToString();
+            }
+        }
+
+      
+
     }
-
-    public void SettingTheHighSCore()
-    {
-
-        Debug.Log(LocalTime);
-
-    }
-
-
 }
+
+
