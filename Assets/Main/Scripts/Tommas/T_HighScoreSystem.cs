374d4ae65cb458bc4356d23bea554c1c47079f2b
MSI\Tommas <havremann@gmail.no>
2018-02-15 16:23:59 +0000

    Death counter og time v.1

--- Assets/Main/Scripts/Tommas/T_HighScoreSystem.cs
+++ Assets/Main/Scripts/Tommas/T_HighScoreSystem.cs
@@ -6,36 +6,64 @@ using UnityEngine.UI;
 public class T_HighScoreSystem : MonoBehaviour {
 
     [SerializeField] private float currentTimeOnThisLevel = 1 ;
- 
 
-    public bool IsTimerOn; 
 
+    public int deathAmount;
+    public bool IsTimerOn;
+    public GameObject GameOverText;
     public Text text;
     public Text Highscore;
+    public Text Deathcounter;
+    public bool DevResetNBThisWillResetAll; 
 
-
+  
     private void Start()
     {
-        Highscore.text = PlayerPrefs.GetFloat("Highscore", 1).ToString();   
+
+        Highscore.text = PlayerPrefs.GetFloat("Highscore", 1).ToString();
+        Deathcounter.text = PlayerPrefs.GetInt("Deathcounter",deathAmount).ToString();
+        deathAmount = PlayerPrefs.GetInt("Deathcounter", deathAmount);
+    
     }
     
     void Update()
     {
-        
+       
+        if (Input.GetKeyDown(KeyCode.Space))
+        {    
+
+
+            deathAmount++;
+        }
+        if (deathAmount > PlayerPrefs.GetInt("Deathcounter",deathAmount))
+        {
+            Deathcounter.text = deathAmount.ToString();
+            PlayerPrefs.SetInt("Deathcounter", deathAmount);
+            
+        }
+
+       
         if (IsTimerOn == true)
         {
             currentTimeOnThisLevel = Time.time;
             currentTimeOnThisLevel = Mathf.Round(currentTimeOnThisLevel);
             text.text = currentTimeOnThisLevel.ToString();
+
             if (currentTimeOnThisLevel > PlayerPrefs.GetFloat("Highscore", 1))
             {
                 PlayerPrefs.SetFloat("Highscore", currentTimeOnThisLevel);
                 Highscore.text = currentTimeOnThisLevel.ToString();
             }
         }
+    }
 
-      
-
+    public void Reset()
+    {
+        if(DevResetNBThisWillResetAll == true)
+        {
+            PlayerPrefs.DeleteAll();
+            //Highscore.ToString;
+        }
     }
 }
 
---
+++ Assets/ManageDeaths.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(GameObject))]
+
+public class ManageDeaths : MonoBehaviour {
+
+    public T_HighScoreSystem deathAmount;
+    public GameObject YouDiedTextOnCube;
+
+	
+	// Update is called once per frame
+	void Update () {
+       
+	}
+}
