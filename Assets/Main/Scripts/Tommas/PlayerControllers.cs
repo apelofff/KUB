913f2672d736e86c694f2b686e77fb5346c51a40
apelofff <joergenstolt@gmail.com>
2018-02-19 20:20:51 +0100

    Revert "Yooooo some change in the player script"
    This reverts commit 32d686cb3b7859d981709b6491606986a4a01f23.

--- Assets/Main/Scripts/Tommas/PlayerControllers.cs
+++ Assets/Main/Scripts/Tommas/PlayerControllers.cs
@@ -112,9 +112,13 @@ public class PlayerControllers : MonoBehaviour {
 
 
         // SlowMotion state, and aiming state
-       
+        if (ThisRB.isKinematic == true && StopMotion == true && ID == 1 /*&& RotationOposite == false*/)
+        {
+            targetArrow.SetActive(true);
+            transform.Rotate(0, 0, Time.deltaTime * rotationSpeedZ);
+        }
 
-         if (ThisRB.isKinematic == true && StopMotion == true && ID == 1 && RotationOposite == true)
+        else if (ThisRB.isKinematic == true && StopMotion == true && ID == 1 && RotationOposite == true)
         {
             targetArrow.SetActive(true);
             transform.Rotate(0, 0, Time.deltaTime * rotationSpeedZ);
