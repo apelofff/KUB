518a61a16d779711ae6ec07d856e749c2312d70d
MSI\Tommas <havremann@gmail.no>
2018-04-12 17:11:26 +0000

    Some Touch and some scripts("Under progress")

--- Assets/Main/Scripts/Tommas/Enemybehavior/EnemyBehavior.cs
+++ Assets/Main/Scripts/Tommas/Enemybehavior/EnemyBehavior.cs
@@ -63,6 +63,7 @@ public class EnemyBehavior : MonoBehaviour {
         if(other.tag == "Dead"){
             forCreatingState = true;
             ISTargetInRange = false;
+            makeEnemyRed.sharedMaterial = materialForEnemy[0];
         }
     }
 
