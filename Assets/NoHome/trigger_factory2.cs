6ae10f66d2b08240a7d18ca9a1c0723d643049db
apelofff <joergenstolt@gmail.com>
2018-05-25 03:17:10 +0200

    Da er factory 1 med bakgrunn og noe ekstra juice. Har tweaket litt på fargene. Er litt mer dark nå.

--- Assets/NoHome/trigger_factory2.cs
+++ Assets/NoHome/trigger_factory2.cs
@@ -6,11 +6,13 @@ using UnityEngine.Playables;
 public class trigger_factory2 : MonoBehaviour
 {
     public PlayableDirector playableDirector;
+    public AudioSource audio;
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player")
         {
+            audio.Play();
             playableDirector.Play();
             Destroy(gameObject);
         }
